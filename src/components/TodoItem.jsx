f3d83ec3b9878a98606447a3941334f8e49d8390
Anurag Jain <jain.anurag0203@gmail.com>
2024-04-21 19:45:01 +0530

    Designed the UI for INdividual Todo Item and Edit save and delete button logic functionality

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -25,20 +25,35 @@ function TodoItem({ todo, index }) {
   return (
     <>
     <div>Individual Todo Item</div>
-        <div>
+        <div className={`flex h-16 border-4 rounded-xl px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300 text-black 
+          ${
+            isTodoEditable
+              ? "bg-blue-300 border-blue-600"
+              : isChecked
+              ? "bg-green-300 border-green-600"
+              : "bg-red-300 border-red-600"
+          }
+          `}>
         <input
           type="checkbox"
+          className="w-8"
           checked={isChecked}
           onChange={toggleCheck}
           disabled={isTodoEditable}
         />
         <input
           type="text"
-          
+          className={`border text-xl outline-none w-full rounded-xl ${
+            isTodoEditable
+              ? "border-slate-800 border-2 px-2 text-cyan-300 bg-slate-900"
+              : "bg-transparent border-transparent cursor-grab"
+          }`}
           style={
             isChecked
               ? {
                   textDecoration: "line-through",
+                  textDecorationColor: "black",
+                  textDecorationThickness: "3px",
                 }
               : null
           }
@@ -46,7 +61,9 @@ function TodoItem({ todo, index }) {
           onChange={(e) => setTodoMsg(e.target.value)}
           readOnly={!isTodoEditable}
         />
-        <button onClick={() => {
+        <button
+        className="inline-flex w-10 h-10 rounded-lg text-sm border bg-yellow-100 border-black/10 justify-center items-center hover:bg-yellow-300 shrink-0 disabled:opacity-50"
+         onClick={() => {
             if (!todoMsg.trim()) {
               setTodoMsg("");
               return;
@@ -66,6 +83,7 @@ function TodoItem({ todo, index }) {
           )}
         </button>
         <button
+        className="inline-flex w-10 h-10 rounded-lg text-sm border border-black/10 justify-center items-center bg-yellow-100 hover:bg-yellow-300 shrink-0"
           onClick={() => deleteTodo(todo.id)}
         >
           <i className="fa fa-trash text-xl"></i>
