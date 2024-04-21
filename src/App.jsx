b93737614ea8d53c4f5a632feac96305d72e40de
Anurag Jain <jain.anurag0203@gmail.com>
2024-04-21 16:51:18 +0530

    Made Create Todo and Individual Todo Item Component For reusibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import './App.css'
+import { TodoForm, TodoItem } from './components/index'
+import { useState, useEffect } from 'react'
+import { TodoProvider } from './api-contexts/TodoContext'
+
+
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
   
   return (
     <>
