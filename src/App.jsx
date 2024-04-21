8d4230ab29185991ab4497b81d4c3b811ede8ed5
Anurag Jain <jain.anurag0203@gmail.com>
2024-04-21 16:58:57 +0530

    Made Functions That go into context Provider that help us make changes in out state

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,38 @@ import { TodoProvider } from './api-contexts/TodoContext'
 function App() {
 
   const [todos, setTodos] = useState([]);
+
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodoCheck = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, checked: !prevTodo.checked }
+          : prevTodo
+      )
+    );
+  };
+
   
   return (
-    <>
-    </>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleTodoCheck}}>
+      
+
+    </TodoProvider>
   )
 }
 
