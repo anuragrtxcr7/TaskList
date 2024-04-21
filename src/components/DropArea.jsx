fd8287819bc41a760d93e9864dbbcd36ce6c13c6
Anurag Jain <jain.anurag0203@gmail.com>
2024-04-22 03:07:59 +0530

    Made the Drop Area Component

--- src/components/DropArea.jsx
+++ src/components/DropArea.jsx
@@ -3,7 +3,22 @@ import { useState } from "react";
 
 function DropArea() {
   return (
-    <div>DropArea</div>
+    <div
+      onDragEnter={() => setShowDrop(true)}
+      onDragLeave={() => setShowDrop(false)}
+      onDrop={() => {
+        onDrop();
+        setShowDrop(false);
+      }}
+      onDragOver={(e) => e.preventDefault()}
+      className={`${
+        showDrop
+          ? "flex h-16 border-4 text-center text-3xl justify-center items-center border-purple-600 bg-purple-400 rounded-lg px-3 py-1.5 gap-x-3 my-2 shadow-sm shadow-white/50 duration-300 text-black"
+          : "opacity-0"
+      }`}
+    >
+      Drop
+    </div>
   )
 }
 
