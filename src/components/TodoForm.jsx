42c82ed67425edb82897bdf6e660d0f8f6902080
Anurag Jain <jain.anurag0203@gmail.com>
2024-04-21 19:41:21 +0530

    Designed the UI of Input bar and create Task Button

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -21,15 +21,17 @@ const add = (e) =>{
   return (
     <>
     <div>TodoForm</div>
-    <form onSubmit={add}>
+    <form onSubmit={add} className="flex">
       <input
         type="text"
         placeholder="Write Todo..."
+        className="w-full border-2 text-xl text-cyan-300 bg-slate-900 h-14 placeholder-cyan-400 border-slate-900 rounded-l-xl rounded-r-none px-4 outline-none duration-150 py-1.5"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
       />
       <button
-        type="submit" /* submit a non-empty todo */
+        type="submit"
+        className="text-white bg-indigo-500 border-2 border-gray-900 px-6 text-2xl focus:outline-none hover:bg-indigo-600 rounded-l-none rounded-r-xl" /* submit a non-empty todo */
       >
         Add
       </button>
